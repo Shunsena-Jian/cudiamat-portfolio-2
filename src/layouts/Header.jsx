9d60bc89d7260dd6603ef80f071276207615dfec
Jian Raphael R. Cudiamat <jian.r.cudiamat@gmail.com>
2025-05-19 15:55:16 +0800

    Updated css styles to modules css

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,37 +1,37 @@
 import githubLogo from '/src/assets/images/github-logo-white.png';
 import linkedInLogo from '/src/assets/images/InBug-White.png';
 
-import '/src/styles/header.css';
+import styles from '/src/styles/header.module.css';
 
 export default function Header() {
     return (
         <section className={'main-section between d-flex'}>
             <div className={'container-fluid m-0 p-5'}>
                 <div className={'row mb-3'}>
-                    <div className={'text-container col-12 col-md-6'}>
-                        <span className={'text-item underline-grow mb-3'}>
+                    <div className={`${styles.textContainer} col-12 col-md-6`}>
+                        <div className={`d-inline-block ${styles.textItem} ${styles.underlineGrow} mb-3`}>
                             <h3 className={'m-0'}>Jian Raphael R. Cudiamat</h3>
-                        </span>
+                        </div>
                         <br/>
-                        <span className={'text-item underline-grow mb-3'}>
+                        <div className={`d-inline-block ${styles.textItem} ${styles.underlineGrow} mb-3`}>
                             <p>Experience</p>
-                        </span>
+                        </div>
                         <br/>
-                        <span className={'text-item underline-grow mb-3'}>
+                        <div className={`d-inline-block ${styles.textItem} ${styles.underlineGrow} mb-3`}>
                             <p>Projects</p>
-                        </span>
+                        </div>
                     </div>
                 </div>
                 <div className={'row'}>
-                    <div className={'logo-container col d-flex flex-wrap gap-4'}>
-                        <a href={'https://github.com/Shunsena-Jian'} target={'_blank'} className={'logo-item'}>
-                            <img src={githubLogo} className={'logo'}/>
+                    <div className={`${styles.logoContainer} col d-flex flex-wrap gap-4`}>
+                        <a href={'https://github.com/Shunsena-Jian'} target={'_blank'} className={`${styles.logoItem}`}>
+                            <img src={githubLogo} className={`${styles.logo}`}/>
                         </a>
-                        <a href={'https://www.linkedin.com/in/jian-raphael-cudiamat-70b1a5269/'} target={'_blank'} className={'logo-item'}>
-                            <img src={linkedInLogo} className={'logo'}/>
+                        <a href={'https://www.linkedin.com/in/jian-raphael-cudiamat-70b1a5269/'} target={'_blank'} className={`${styles.logoItem}`}>
+                            <img src={linkedInLogo} className={`${styles.logo}`}/>
                         </a>
-                        {/*<a href={'https://github.com/Shunsena-Jian'} target={'_blank'} className={'logo-item'}>*/}
-                        {/*    <img src={githubLogo} className={'logo'}/>*/}
+                        {/*<a href={'https://github.com/Shunsena-Jian'} target={'_blank'} className={`${styles.logoItem}`}>*/}
+                        {/*    <img src={githubLogo} className={`${styles.logo}`}/>*/}
                         {/*</a>*/}
                     </div>
                 </div>
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,16 @@
-import '/src/styles/main.css';
+import styles from '/src/styles/main.module.css';
 
 export default function Main() {
     return (
         <section className={'main-section min-vh-100 vw-100'}>
+            <div className={'row'}>
+                <div className={'col-12 col-md-4'}>
 
+                </div>
+                <div className={'col-12 col-md-8'}>
+
+                </div>
+            </div>
         </section>
     )
 }
