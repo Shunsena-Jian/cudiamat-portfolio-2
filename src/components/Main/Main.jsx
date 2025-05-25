c310125c9c54d49adea23b351c48416da94abb86
Jian Raphael R. Cudiamat <jian.r.cudiamat@gmail.com>
2025-05-25 15:21:15 +0800

    Refactor Main and AppLayout components for cleaner structure

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import laravelLogo from '/src/assets/images/laravel-logo.png';
 import bootstrapLogo from '/src/assets/images/bootstrap.png';
 import javaLogo from '/src/assets/images/java.png';
-import jQyeryLogo from '/src/assets/images/jQuery.png';
+import jQueryLogo from '/src/assets/images/jQuery.png';
 import mySqlLogo from '/src/assets/images/mySQL.png';
 import nodeJSLogo from '/src/assets/images/nodeJS.png';
 import reactLogo from '/src/assets/images/react.png';
@@ -10,31 +10,134 @@ import styles from '/src/styles/main.module.css';
 
 import MainLayout from './MainLayout.jsx';
 
+const stylesLogo = `${styles.logo}`
+const imageElement = 'img';
+const pElement = 'p';
+
+const logoItems = [
+    {
+        id: 'laravel_logo',
+        ElementType: imageElement,
+        src: laravelLogo,
+        className: stylesLogo
+    },
+    {
+        id: 'bootstrap_logo',
+        ElementType: imageElement,
+        src: bootstrapLogo,
+        className: stylesLogo
+    },
+    {
+        id: 'java_logo',
+        ElementType: imageElement,
+        src: javaLogo,
+        className: stylesLogo
+    },
+    {
+        id: 'jQuery_logo',
+        ElementType: imageElement,
+        src: jQueryLogo,
+        className: stylesLogo
+    },
+    {
+        id: 'mySQL_logo',
+        ElementType: imageElement,
+        src: mySqlLogo,
+        className: stylesLogo
+    },
+    {
+        id: 'nodeJS_logo',
+        ElementType: imageElement,
+        src: nodeJSLogo,
+        className: stylesLogo
+    },
+    {
+        id: 'react_logo',
+        ElementType: imageElement,
+        src: reactLogo,
+        className: stylesLogo
+    }
+];
+
+const paragraphs = [
+    {
+        key: 1,
+        ElementType: pElement,
+        content: 'Based in the Philippines'
+    },
+    {
+        key: 2,
+        ElementType: pElement,
+        content: 'I am a dedicated'
+    },
+    {
+        key: 3,
+        ElementType: pElement,
+        content: 'Full Time Backend Developer'
+    },
+    {
+        key: 4,
+        ElementType: pElement,
+        content: 'with expertise in the Laravel framework'
+    },
+    {
+        key: 5,
+        ElementType: pElement,
+        content: 'passionate about building robust'
+    },
+    {
+        key: 6,
+        ElementType: pElement,
+        content: 'and efficient server-side applications.'
+    },
+    {
+        key: 7,
+        ElementType: pElement,
+        content: 'Driven by a desire to'
+    },
+    {
+        key: 8,
+        ElementType: pElement,
+        content: 'broaden my technical horizons,'
+    },
+    {
+        key: 9,
+        ElementType: pElement,
+        content: 'I am actively pursuing growth to evolve'
+    },
+    {
+        key: 10,
+        ElementType: pElement,
+        content: 'into a versatile Full Stack Developer.'
+    },
+];
+
 function Main() {
     const leftContent = (
         <div className={`${styles.logoContainer} d-flex flex-wrap`}>
-            <img src={laravelLogo} className={`${styles.logo}`}/>
-            <img src={bootstrapLogo} className={`${styles.logo}`}/>
-            <img src={javaLogo} className={`${styles.logo}`}/>
-            <img src={jQyeryLogo} className={`${styles.logo}`}/>
-            <img src={mySqlLogo} className={`${styles.logo}`}/>
-            <img src={nodeJSLogo} className={`${styles.logo}`}/>
-            <img src={reactLogo} className={`${styles.logo}`}/>
+            {
+                logoItems.map(item => {
+                    const {id, ElementType, src, className} = item;
+
+                    return (
+                        <ElementType key={id} src={src} className={className}/>
+                    );
+                })
+            }
         </div>
     );
 
     const rightContent = (
         <div className={`${styles.tenPx} ${styles.animateFromRight} text-end`}>
-            <p>Based in the Philippines</p>
-            <p>I am a dedicated</p>
-            <p>Full Time Backend Developer</p>
-            <p>with expertise in the Laravel framework</p>
-            <p>passionate about building robust</p>
-            <p>and efficient server-side applications.</p>
-            <p>Driven by a desire to</p>
-            <p>broaden my technical horizons,</p>
-            <p>I am actively pursuing growth to evolve</p>
-            <p>into a versatile Full Stack Developer.</p>
+            {
+                paragraphs.map(item => {
+                    const {id, ElementType, content} = item;
+
+                    return (
+                        <ElementType key={id}>{content}</ElementType>
+                    );
+                })
+            }
         </div>
     );
 
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -2,11 +2,13 @@ import Header from '/src/components/Header/Header.jsx';
 
 function AppLayout({ header, content }) {
     return (
-        <div className={'container-fluid vh-100 p-0'}>
-            { header }
+        <div className={'container-fluid d-flex flex-column vh-100 p-0'}>
+            <div>
+                { header }
+            </div>
 
             {/* Main Content Row */}
-            <div className={'row g-0 flex-grow-1'}>
+            <div className={'row g-0 flex-grow-1 align-items-end'}>
                 { content }
             </div>
         </div>
