c8f780cd1e25cfb9e2a22081e96000cf10da1f87
Jian Raphael R. Cudiamat <jianraphael@qualitytrade.com>
2025-06-09 06:52:09 +0800

    Refactor paragraph structure for improved readability and maintainability

--- src/constants/MainContent/rightMainContent.js
+++ src/constants/MainContent/rightMainContent.js
@@ -1,54 +1,23 @@
-const pElement = 'p';
+const paragraphProps = {
+    ElementType: 'p',
+};
 
-export const paragraphs = [
-    {
-        key: 1,
-        ElementType: pElement,
-        content: 'Based in the Philippines'
-    },
-    {
-        key: 2,
-        ElementType: pElement,
-        content: 'I am a dedicated'
-    },
-    {
-        key: 3,
-        ElementType: pElement,
-        content: 'Full Time Backend Developer'
-    },
-    {
-        key: 4,
-        ElementType: pElement,
-        content: 'with expertise in the Laravel framework'
-    },
-    {
-        key: 5,
-        ElementType: pElement,
-        content: 'passionate about building robust'
-    },
-    {
-        key: 6,
-        ElementType: pElement,
-        content: 'and efficient server-side applications.'
-    },
-    {
-        key: 7,
-        ElementType: pElement,
-        content: 'Driven by a desire to'
-    },
-    {
-        key: 8,
-        ElementType: pElement,
-        content: 'broaden my technical horizons,'
-    },
-    {
-        key: 9,
-        ElementType: pElement,
-        content: 'I am actively pursuing growth to evolve'
-    },
-    {
-        key: 10,
-        ElementType: pElement,
-        content: 'into a versatile Full Stack Developer.'
-    },
+const paragraphContents = [
+    'Based in the Philippines',
+    'I am dedicated Full Time',
+    'Backend Developer with',
+    'expertise in the Laravel',
+    'framework passionate about',
+    'building robust and efficient',
+    'server-side applications.',
+    'Driven by a desire to broaden',
+    'my technical horizons, I am',
+    'actively pursuing growth to evolve',
+    'into a versatile Full Stack Developer.'
 ];
+
+export const paragraphs = paragraphContents.map((text, index) => ({
+    key: index + 1,
+    content: text,
+    ...paragraphProps
+}));
