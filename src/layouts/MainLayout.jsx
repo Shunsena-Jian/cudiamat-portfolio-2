8862579d8ea505e3e2b506fbf87289a35a12e65c
Jian Raphael R. Cudiamat <jianraphael@qualitytrade.com>
2025-06-27 09:24:17 +0800

    Refactor MainLayout: remove `mainContent` prop and simplify content rendering

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -25,21 +25,13 @@ const RightColumnContent = ({rightColumn}) => {
     );
 }
 
-export default function MainLayout({mainContent, upperContent, leftColumn, rightColumn}) {
+export default function MainLayout({upperContent, leftColumn, rightColumn}) {
     return (
         <main className="main-section container-fluid d-flex align-items-center align-items-md-end justify-content-center p-3">
             {/* Main Row */}
             <div className={'row container-fluid p-0'}>
-                {
-                    mainContent
-                        ? mainContent
-                        : (
-                            <>
-                                <LeftColumnContent upperContent={upperContent} leftColumn={leftColumn}/>
-                                <RightColumnContent rightColumn={rightColumn}/>
-                            </>
-                        )
-                }
+                <LeftColumnContent upperContent={upperContent} leftColumn={leftColumn}/>
+                <RightColumnContent rightColumn={rightColumn}/>
             </div>
         </main>
     );
