2bd6c8113246cb0bd4f08fdf11366e52ab5b56ae
Jian Raphael R. Cudiamat <jianraphael@qualitytrade.com>
2025-07-03 13:07:02 +0800

    Refactor ProjectLayout: adjust column widths for left and right sections to improve layout balance.

--- src/layouts/ProjectLayout.jsx
+++ src/layouts/ProjectLayout.jsx
@@ -2,7 +2,7 @@ export default function ProjectLayout({left, middle, right}) {
     return (
         <main className={'project-section container-fluid p-3 d-flex justify-content-start'}>
             <section className={'row container-fluid p-0'}>
-                <div className={'col-2 d-flex align-items-center'}>
+                <div className={'col-3 d-flex align-items-center'}>
                     {
                         left
                             ? left
@@ -16,7 +16,7 @@ export default function ProjectLayout({left, middle, right}) {
                             : <p>No details or confidential</p>
                     }
                 </div>
-                <div className={'col-5 d-flex align-items-center'}>
+                <div className={'col-4 d-flex align-items-center'}>
                     {
                         right
                             ? right
