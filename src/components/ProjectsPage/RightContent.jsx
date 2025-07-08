17ed586f33ffb939b9e39ab79fd124d98f3cb80b
Jian Raphael R. Cudiamat <jianraphael@qualitytrade.com>
2025-07-08 14:43:48 +0800

    Refactor Projects: restructure data to include `middleContent` and `rightContent`, update `ProjectsContext` to manage new fields, and enhance `RightContent` component with dynamic rendering logic.

--- src/components/ProjectsPage/RightContent.jsx
+++ src/components/ProjectsPage/RightContent.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useContext} from "react";
+import {ProjectsContext} from "../../pages/Projects/ProjectsContextProvider.jsx";
 
 const RightContent = () => {
+    const { isActive, rightContent } = useContext(ProjectsContext)
 
+    console.log(rightContent);
 }
 
 export default RightContent;
--- src/pages/Projects/ProjectsContextProvider.jsx
+++ src/pages/Projects/ProjectsContextProvider.jsx
@@ -3,33 +3,45 @@ import React, {createContext, useState} from 'react';
 export const ProjectsContext = createContext();
 
 export const ProjectsProvider = ({ children }) => {
+    const [rightContent, setRightContent] = useState ('');
     const [middleContent, setMiddleContent] = useState('');
     const [isActive, setIsActive] = useState(false);
+    const [hoveredItem, setHandleHoveredItem] = useState('');
 
     const handleMouseOnClick = (item) => {
         const isSameItem = middleContent === item;
 
-        if (middleContent) {
+        if (isActive) {
             setIsActive(false);
             setTimeout(() => {
                 if (isSameItem) {
                     setMiddleContent('');
+                    setRightContent('');
                 } else {
-                    setMiddleContent(item);
+                    setMiddleContent(item.middleContent);
+                    setRightContent(item.rightContent);
                     setIsActive(true);
                 }
             }, 500);
         } else {
-            setMiddleContent(item);
+            setMiddleContent(item.middleContent);
+            setRightContent(item.rightContent);
             setIsActive(true);
         }
+    }
+
+    const handleHoverItem = () => {
+        setHandleHoveredItem(true);
     }
 
     return (
         <ProjectsContext.Provider value={{
             isActive,
             middleContent,
+            rightContent,
+            hoveredItem,
             handleMouseOnClick,
+            handleHoverItem
         }}>
             {children}
         </ProjectsContext.Provider>
