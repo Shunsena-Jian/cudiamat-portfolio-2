669ad5319e8b5e59a2b71c32b391fea8054154f9
Jian Raphael R. Cudiamat <jianraphael@qualitytrade.com>
2025-07-09 14:55:14 +0800

    Refactor `ProjectsContext` and `RightContent`: remove `isActiveMiddle` from `RightContent` and clean up unused state.

--- src/components/ProjectsPage/RightContent.jsx
+++ src/components/ProjectsPage/RightContent.jsx
@@ -3,7 +3,7 @@ import {ProjectsContext} from "../../pages/Projects/ProjectsContextProvider.jsx"
 import styles from "../../styles/projects.module.css";
 
 const RightContent = () => {
-    const { isActiveRight, isActiveMiddle, rightContent } = useContext(ProjectsContext)
+    const { isActiveRight, rightContent } = useContext(ProjectsContext)
 
     return(
         <div className={`d-flex flex-column gap-1 ${isActiveRight ? styles.middleContentActive : styles.middleContentHide}`}>
