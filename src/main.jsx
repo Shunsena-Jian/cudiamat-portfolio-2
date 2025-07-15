23c035b300186aae2c1f32b1323edcc1d24156d4
Jian Raphael R. Cudiamat <jian.r.cudiamat@gmail.com>
2025-07-15 14:26:33 +0800

    Sample commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
