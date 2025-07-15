fb0da08ea56b696f04ba7cb1880714efee7b1116
Jian Raphael R. Cudiamat <jianraphael@qualitytrade.com>
2025-07-15 08:45:23 +0800

    Update project technology representation: replace technology names with corresponding logo images in projects.json and enhance RightContent component to display technology logos with new styling.

--- src/components/ProjectsPage/RightContent.jsx
+++ src/components/ProjectsPage/RightContent.jsx
@@ -4,6 +4,17 @@ import styles from "../../styles/projects.module.css";
 import pmaLogo from "/src/assets/images/companies/pma_logo.png";
 import iThinkWebLogo from "/src/assets/images/companies/ithinkweb_corporation_logo.png";
 import sluLogo from "/src/assets/images/companies/slu_logo.png";
+import laravelLogo from "/src/assets/images/technologies/laravel-logo.png";
+import mySqlLogo from "/src/assets/images/technologies/mySQL.png";
+import mongoDBLogo from "/src/assets/images/technologies/mongoDB.png";
+import dynamoDBLogo from "/src/assets/images/technologies/dynamoDB.svg";
+import opensearchLogo from "/src/assets/images/technologies/opensearch.svg";
+import javascriptLogo from "/src/assets/images/technologies/javascript_logo.png";
+import htmlLogo from "/src/assets/images/technologies/html_logo.png";
+import nodeJSLogo from "/src/assets/images/technologies/nodeJS.png";
+import pugLogo from "/src/assets/images/technologies/pug_logo.png";
+import cssLogo from "/src/assets/images/technologies/css_logo.png";
+import tailWindLogo from "/src/assets/images/technologies/tailwind_logo.png";
 
 const companyLogos = {
     'slu_logo.png': sluLogo,
@@ -11,8 +22,26 @@ const companyLogos = {
     'ithinkweb_logo.png': iThinkWebLogo
 };
 
+const techStackLogos = {
+    'laravel-logo.png': laravelLogo,
+    'mySQL.png': mySqlLogo,
+    'mongoDB.png': mongoDBLogo,
+    'dynamoDB.svg': dynamoDBLogo,
+    'opensearch.svg': opensearchLogo,
+    'javascript_logo.png': javascriptLogo,
+    'html_logo.png': htmlLogo,
+    'nodeJS.png': nodeJSLogo,
+    'pug_logo.png': pugLogo,
+    'css_logo.png': cssLogo,
+    'tailwind_logo.png': tailWindLogo
+};
+
 const RightContent = () => {
     const { isActiveRight, rightContent } = useContext(ProjectsContext);
+
+    if (!rightContent) {
+        return null;
+    }
 
     const company = rightContent.src ? companyLogos[rightContent.src] : null;
 
@@ -29,12 +58,13 @@ const RightContent = () => {
                     </div>
                 ) : ("")
             }
-            <div className={'d-flex gap-1'}>
-                {/*{*/}
-                {/*    rightContent.technology.map((tech, index) => {*/}
-                {/*        console.log(tech);*/}
-                {/*    })*/}
-                {/*}*/}
+            <div className={'d-flex gap-1 justify-content-center'}>
+                {
+                    rightContent.technology && rightContent.technology.map((tech, index) => {
+                        const techLogo = techStackLogos[tech];
+                        return techLogo ? <img key={index} src={techLogo} alt={tech} className={styles.techLogo}/> : null;
+                    })
+                }
             </div>
         </div>
     );
