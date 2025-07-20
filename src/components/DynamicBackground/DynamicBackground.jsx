c9d7dc441420d162a7504b8a809f04d0ea626bc6
Jian Raphael R. Cudiamat <jian.r.cudiamat@gmail.com>
2025-07-20 15:49:35 +0800

    Update styling: change default backgrounds and text colors across components to improve contrast and visual consistency

--- src/components/DynamicBackground/DynamicBackground.jsx
+++ src/components/DynamicBackground/DynamicBackground.jsx
@@ -8,16 +8,16 @@ export default function DynamicBackground() {
         const body = document.querySelector('body');
         switch (location.pathname) {
             case '/':
-                body.style.background = 'linear-gradient(150deg, rgba(0, 59, 117, 1) 0%, rgba(0, 0, 0, 1) 30%)';
+                body.style.background = 'linear-gradient(150deg, rgba(0, 59, 117, 1) 0%, rgba(255, 255, 255, 1) 30%)';
                 break;
             case '/projects':
-                body.style.background = 'linear-gradient(150deg, rgba(117, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 30%)';
+                body.style.background = 'linear-gradient(150deg, rgba(117, 0, 0, 1) 0%, rgba(255, 255, 255, 1) 30%)';
                 break;
             case '/experience':
-                body.style.background = 'linear-gradient(150deg, rgba(0, 117, 0, 1) 0%, rgba(0, 0, 0, 1) 30%)';
+                body.style.background = 'linear-gradient(150deg, rgba(0, 117, 0, 1) 0%, rgba(255, 255, 255, 1) 30%)';
                 break;
             default:
-                body.style.background = 'linear-gradient(150deg, rgba(0, 59, 117, 1) 0%, rgba(0, 0, 0, 1) 30%)';
+                body.style.background = 'linear-gradient(150deg, rgba(0, 59, 117, 1) 0%, rgba(255, 255, 255, 1) 30%)';
                 break;
         }
     }, [location]);
