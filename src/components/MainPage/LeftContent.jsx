8fafa8aff83e3880d54558d0f96edf4948085ee6
Jian Raphael R. Cudiamat <jian.r.cudiamat@gmail.com>
2025-08-05 09:09:27 +0800

    Enhance LeftContent component: add new technology logos and refactor logo rendering for improved maintainability and clarity

--- src/components/MainPage/LeftContent.jsx
+++ src/components/MainPage/LeftContent.jsx
@@ -4,32 +4,6 @@ import styles from '../../styles/main.module.css';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
-import javaLogo from "../../assets/images/technologies/java.png";
-import pythonLogo from "../../assets/images/technologies/python.png";
-import nodeJSLogo from "../../assets/images/technologies/nodeJS.png";
-import laravelLogo from "../../assets/images/technologies/laravel-logo.png";
-import mySqlLogo from "../../assets/images/technologies/mySQL.png";
-import mongoDBLogo from "../../assets/images/technologies/mongoDB.png";
-import dynamoDB from "../../assets/images/technologies/dynamoDB.svg";
-import opensearch from "../../assets/images/technologies/opensearch.svg";
-import reactLogo from "../../assets/images/technologies/react.png";
-import bootstrapLogo from "../../assets/images/technologies/bootstrap.png";
-import jQueryLogo from "../../assets/images/technologies/jQuery.png";
-
-const logoImages = {
-    'java.png': javaLogo,
-    'python.png': pythonLogo,
-    'nodeJS.png': nodeJSLogo,
-    'laravel-logo.png': laravelLogo,
-    'mySQL.png': mySqlLogo,
-    'mongoDB.png': mongoDBLogo,
-    'dynamoDB.svg': dynamoDB,
-    'opensearch.svg': opensearch,
-    'react.png': reactLogo,
-    'bootstrap.png': bootstrapLogo,
-    'jQuery.png': jQueryLogo,
-};
-
 const LeftContent = () => {
     const { beTech, feTech, dbTech, handleAnimationEnd } = useContext(MainContext);
 
@@ -39,71 +13,34 @@ const LeftContent = () => {
         </Tooltip>
     );
 
+    const renderLogoSection = (title, technologies) => (
+        <div>
+            <h5>{title}</h5>
+            <div className={`${styles.logoContainer} d-flex flex-wrap gap-2 mt-2`}>
+                {technologies.map((tech, index) => {
+                    const { id, src, alt } = tech;
+                    const imageUrl = new URL(`../../assets/images/technologies/${src}`, import.meta.url).href;
+
+                    return (
+                        <OverlayTrigger
+                            key={id}
+                            placement="top"
+                            delay={{ show: 0, hide: 250 }}
+                            overlay={(props) => renderTooltip(props, alt)}
+                        >
+                            <img src={imageUrl} alt={alt} onAnimationEnd={handleAnimationEnd} className={styles.logo} style={{'--n': index + 1}} />
+                        </OverlayTrigger>
+                    );
+                })}
+            </div>
+        </div>
+    );
+
     return (
         <div className={'d-flex flex-column gap-2'}>
-            <div>
-                <h5>Server Side</h5>
-                <div className={`${styles.logoContainer} d-flex flex-wrap gap-2 mt-2`}>
-                    {
-                        beTech.map((backend, index) => {
-                            const { id, src, alt } = backend;
-
-                            return (
-                                <OverlayTrigger
-                                    key={id}
-                                    placement="top"
-                                    delay={{ show: 0, hide: 250 }}
-                                    overlay={(props) => renderTooltip(props, alt)}
-                                >
-                                    <img src={logoImages[src]} alt={alt} onAnimationEnd={handleAnimationEnd} className={styles.logo} style={{'--n': index + 1}} />
-                                </OverlayTrigger>
-                            );
-                        })
-                    }
-                </div>
-            </div>
-            <div>
-                <h5>Frontend</h5>
-                <div className={`${styles.logoContainer} d-flex flex-wrap gap-2 mt-2`}>
-                    {
-                        feTech.map((frontend, index) => {
-                            const { id, src, alt } = frontend;
-
-                            return (
-                                <OverlayTrigger
-                                    key={id}
-                                    placement="top"
-                                    delay={{ show: 0, hide: 250 }}
-                                    overlay={(props) => renderTooltip(props, alt)}
-                                >
-                                    <img src={logoImages[src]} alt={alt} onAnimationEnd={handleAnimationEnd} className={styles.logo} style={{'--n': index + 1}} />
-                                </OverlayTrigger>
-                            );
-                        })
-                    }
-                </div>
-            </div>
-            <div>
-                <h5>Database</h5>
-                <div className={`${styles.logoContainer} d-flex flex-wrap gap-2 mt-2`}>
-                    {
-                        dbTech.map((database, index) => {
-                            const { id, src, alt } = database;
-
-                            return (
-                                <OverlayTrigger
-                                    key={id}
-                                    placement="top"
-                                    delay={{ show: 0, hide: 250 }}
-                                    overlay={(props) => renderTooltip(props, alt)}
-                                >
-                                    <img src={logoImages[src]} alt={alt} onAnimationEnd={handleAnimationEnd} className={styles.logo} style={{'--n': index + 1}} />
-                                </OverlayTrigger>
-                            );
-                        })
-                    }
-                </div>
-            </div>
+            {renderLogoSection('Server Side', beTech)}
+            {renderLogoSection('Frontend', feTech)}
+            {renderLogoSection('Database', dbTech)}
         </div>
     );
 };
