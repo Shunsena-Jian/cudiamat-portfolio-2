b013a9b5dc9b00b71a99c0e5e81249f4c1c1f9d7
Jian Raphael R. Cudiamat <jian.r.cudiamat@gmail.com>
2025-08-06 08:45:04 +0800

    Refactor ExperienceLayout component: implement grid layout and update styling

--- src/layouts/ExperienceLayout.jsx
+++ src/layouts/ExperienceLayout.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import experienceStyles from '../styles/experiences.module.css';
 
 export default function ExperienceLayout({ content, details }) {
     return (
-        <main className={'experience-section d-flex p-3'}>
-            <section className={'row container-fluid'}>
-                <div className={'col-4 d-flex align-items-center'}>
-                    {
-                        content
-                            ? content
-                            : <p>No experiences to show for now.</p>
-                    }
-                </div>
-                <div className={'col-8 d-flex align-items-center justify-content-start'}>
-                    {
-                        details
-                            ? details
-                            : <p>No details or confidential</p>
-                    }
-                </div>
-            </section>
+        <main className={experienceStyles['grid-container']}>
+            <div className={`${experienceStyles['left-column']} align-items-center`}>
+                {
+                    content
+                        ? content
+                        : <p>No experiences to show for now.</p>
+                }
+            </div>
+            <aside className={`${experienceStyles.gridItem} d-flex align-items-center`}>
+                {
+                    details
+                        ? details
+                        : <p>No details or confidential</p>
+                }
+            </aside>
         </main>
     );
 }
